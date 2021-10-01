fe11cec082ea91a7ed9a72daf01a627cfbc855b1
joswald123 <48632836+joswald123@users.noreply.github.com>
2021-10-01 15:33:10 -0700

    implementacion del ajax method GET. creacion de tablas y formularios HTML

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,36 +1,39 @@
-/* esta funcion llama la funcion pintar respuesta que tiene la tabla para mostrar y llama los datos de la tabla cuando damoos click boton consultar */
-
-function traerInformacion(){
-    $.ajax({
-        url:"https://g9004d44ee12137-db202109240616.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/machine/machine",
-        type:"GET",
-        datatype:"JSON",
-        success:function(respuesta){
-            console.log(respuesta);
-            pintarRespuesta(respuesta.items);
-    
-        }
-    
-    });
-
-}
-
-/*muestras los datos en la tabla*/
-
-function pintarRespuesta(items){
-    let myTable="<table>"
+// Funcion para crear las filas de la tabla alimentada por la funcion machineTable 
+var createRow = function(items) {
+    // creamos unanueva fila en la tabla
     for(i=0;i<items.length;i++){
-        myTable+="<tr>";
-        myTable+="<td>"+items[i].id+"<td>";
-        myTable+="<td>"+items[i].name+"<td>";
-        myTable+="<td>"+items[i].brand+"<td>";
-        myTable+="<td>"+items[i].model+"<td>";
-        myTable+="<td>"+items[i].category_id+"<td>";
-        myTable+="<td> <button onclick='borrarElemento("+items[i].id+")'>Borrar</button>";
-    }		
+        
+        var tRow = $("<tr>");
 
-    myTable+="</table>" 
-    $("#resultado").append(myTable);
+        var idTd = $("<td>").text(items[i].id);
+        var nameTd = $("<td>").text(items[i].name);
+        var brandTd = $("<td>").text(items[i].brand);
+        var modelTd = $("<td>").text(items[i].model);
+        var categoryIdTd = $("<td>").text(items[i].category_id);
+        
 
+        tRow.append(idTd);
+        tRow.append(nameTd);
+        tRow.append(brandTd);
+        tRow.append(modelTd);
+        tRow.append(categoryIdTd);
+        
+        $("tbody").append(tRow)
+
+    }
+    
 }
+// Funcion que consulta toda la info de la tabla machine SQL Cloud
+var machineTable = function(){
+    var queryURL = "https://g9004d44ee12137-db202109240616.adb.us-sanjose-1.oraclecloudapps.com/ords/admin/machine/machine";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        createRow(response.items);
+        console.log(response.items);
+    });
+};
+
+machineTable()
 
